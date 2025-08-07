0d67ce2aae3e9bea41c79a0e88c4b8c949890ccb
KhushiUp-adhyay <123657250+KhushiUp-adhyay@users.noreply.github.com>
2025-08-07 04:13:16 +0000

    container config done

---
+++ basic/dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node.js, not browser
+
+console.log(5+5)
+// code readability acchi honi chahiye
+console.log("Pippo")
+
+let name = "Pippoo"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 se power 53 tak
+// bigint => bahut bade numbers k liye
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined => define nhi ki gayi
+// symbol => unique batane k liye
+
+// object
+
+console.log(typeof "Pippo"); //string
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
