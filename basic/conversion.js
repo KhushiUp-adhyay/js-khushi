bbf2ee0fa85227bd5043f438c5fd205f0ea6d689
KhushiUp-adhyay <123657250+KhushiUp-adhyay@users.noreply.github.com>
2025-08-07 08:14:21 +0000

    container config done

---
+++ basic/conversion.js
@@ -0,0 +1,26 @@
+
+// let score = "33a"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33a" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "Pippo" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+
